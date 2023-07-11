8e27eb6e96025ad2a9e9a5b609578725e65b61c0
Chetan Deshpande <chetan.deshpande@protonmail.com>
2023-07-11 13:35:54 +0530

    updated hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,40 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const POLYGON_API_KEY = process.env.POLYGON_API_KEY;
+const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL;
+
+console.log(PRIVATE_KEY, POLYGON_API_KEY, POLYGON_RPC_URL);
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: '0.8.18',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {},
+    polygonMainnet: {
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/_ULp5HCwK_YWhB3OfsvTU64A8G9A0KsY',
+      account: [PRIVATE_KEY],
+    },
+
+    polygonTestnet: {
+      url: POLYGON_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.POLYGON_API_KEY,
+  },
 };
 
 export default config;
